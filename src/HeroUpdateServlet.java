671ba4a7d35514569102302ccd4458063e1f4faa
simba426 <isxinzq@hotmail.com>
2019-07-12 12:28:39 +0800

    finsh CRUD all

---
+++ src/HeroUpdateServlet.java
@@ -0,0 +1,20 @@
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+public class HeroUpdateServlet extends HttpServlet {
+    protected void service (HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+
+        Hero hero = new Hero();
+        hero.setId(Integer.parseInt(request.getParameter("id")));
+        hero.setName(request.getParameter("name"));
+        hero.setHp(Float.parseFloat(request.getParameter("hp")));
+        hero.setDamage(Integer.parseInt(request.getParameter("damage")));
+
+        new HeroDAO().update(hero);
+        response.sendRedirect("/listHero");
+    }
+}
